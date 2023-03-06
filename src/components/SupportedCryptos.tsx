ceda5bbbbdb9e58ece52f4029dfa54164b1a0977
amirmahdi <amirmahdikeshanzare@gmail.com>
2023-03-06 23:23:08 +0330

    Delete Unnessary Codes

--- src/components/SupportedCryptos.tsx
+++ src/components/SupportedCryptos.tsx
@@ -22,9 +22,6 @@ export const SupportedCryptos = () => {
                         <TableRow>
                             <TableCell>#</TableCell>
                             <TableCell align="left">Crypto Name</TableCell>
-                            {/* <TableCell align="right">Fat&nbsp;(g)</TableCell>
-                        <TableCell align="right">Carbs&nbsp;(g)</TableCell>
-                        <TableCell align="right">Protein&nbsp;(g)</TableCell> */}
                         </TableRow>
                     </TableHead>
                     <TableBody>
@@ -37,9 +34,6 @@ export const SupportedCryptos = () => {
                                     {index}
                                 </TableCell>
                                 <TableCell align="left">{crypto}</TableCell>
-                                {/* <TableCell align="right">{row?.fat}</TableCell>
-                                <TableCell align="right">{row?.carbs}</TableCell>
-                                <TableCell align="right">{row?.protein}</TableCell> */}
                             </TableRow>
                         ))}
                     </TableBody>
